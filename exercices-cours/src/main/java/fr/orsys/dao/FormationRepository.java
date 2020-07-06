d1080099a9379909cd5135bfc7d48d3987ce685e
sofiene bne jamaa <gcr.sofiene@gmail.com>
2020-07-06 20:06:16 +0100

    mise à jours

--- exercices-cours/src/main/java/fr/orsys/dao/FormationRepository.java
+++ exercices-cours/src/main/java/fr/orsys/dao/FormationRepository.java
@@ -1,14 +1,20 @@
 package fr.orsys.dao;
 
 
+import java.awt.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
 import fr.orsys.entities.Formation;
+import fr.orsys.entities.Participant;
+
 
 public interface FormationRepository extends JpaRepository<Formation, Long>{
-
 	public Formation findByCode(String code);
+
+	public Page<Participant> findAllParticipantByCode(String code, PageRequest of);
 
 }
--- exercices-cours/src/main/java/fr/orsys/web/FormationController.java
+++ exercices-cours/src/main/java/fr/orsys/web/FormationController.java
@@ -1,27 +1,23 @@
 package fr.orsys.web;
-
 import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.Date;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import fr.orsys.dao.FormationRepository;
+
 import fr.orsys.entities.Formation;
+;
 
 @Controller
 public class FormationController {
 	@Autowired
 	private FormationRepository formationRepository;
-
-	@RequestMapping(value = "/formations")
-	public String formations() {
-		return "formations";
-	}
 
 	@RequestMapping(value = "/consulterformation")
 	public String consulterformation(Model model, @RequestParam(name = "code", defaultValue = "") String code) {
@@ -32,7 +28,12 @@ public class FormationController {
 		Date time = new Date();
 		DateFormat d = DateFormat.getDateInstance(DateFormat.FULL);
 		model.addAttribute("dateSys", d.format(time));
-		model.addAttribute("formation", f);
+		model.addAttribute("formation", f);	
+		model.addAttribute("listeParticipants",f.getParticipant());
+		return "formations";
+	}
+	@RequestMapping(value = "/formations")
+	public String formations() {
 		return "formations";
 	}
 }
